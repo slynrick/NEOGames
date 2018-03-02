f117f535121b47e6f58a3b0d707733a609371d33
Fabio Henrique Cardoso <slynrick@gmail.com>
2018-03-02 00:11:58 -0300

    changed storage mothod

--- NEOPixelBattle/NEOPixelBattle/pixelbattle.cs
+++ NEOPixelBattle/NEOPixelBattle/pixelbattle.cs
@@ -1,64 +1,59 @@
 ﻿using Neo.SmartContract.Framework;
 using Neo.SmartContract.Framework.Services.Neo;
 using System;
+using System.ComponentModel;
 using System.Numerics;
 
 namespace Neo.SmartContract
 {
     public class NEOPixelBattle : Framework.SmartContract
     {
+        [DisplayName("colored")]
+        public static event Action<BigInteger,BigInteger,byte[]> Colored;
+
         public static object Main(string operation, params object[] args)
         {
-            if (operation == "Create()")
-            {
-                return Create();
-            }
-
             if ( operation == "SetColorRGB()")
             {
-                if (args.Length != 2) return false;
-                return SetColorRGB((int)args[0], (int)args[1], (byte[])args[2]);
+                if (args.Length != 3) return false;
+                return SetColorRGB((byte[])args[0], (byte[])args[1], (byte[])args[2]);
             }
 
-            if (operation == "GetData()")
+            if (operation == "GetColorRGB()")
             {
-                return GetData();
+                if (args.Length != 2) return false;
+                return GetColorRGB((byte[])args[0], (byte[])args[1]);
             }
 
             return false;
         }
 
-        private static byte[] GetData()
-        {
-           return Storage.Get(Storage.CurrentContext, "FirstBattleGound".AsByteArray());
-        }
-
-        private static byte[] SetColorRGB(int i, int j, byte[] color)
+        private static bool SetColorRGB(byte[] i, byte[] j, byte[] color)
         {
             if (color.Length != 3)
-                return null;
-            if (i > 1000 || j > 1000)
-                return null;
+                return false;
+            if (i.AsBigInteger() > 3000 || j.AsBigInteger() > 3000)
+                return false;
 
-            byte[] data = Storage.Get(Storage.CurrentContext, "FirstBattleGound".AsByteArray());
-            data[i * 3000 + j] = color[0];
-            data[i * 3000 + j + 1] = color[1];
-            data[i * 3000 + j + 2] = color[2];
-            return null;
+            byte[] key = new byte[] { };
+            key.Concat("FirstBattleGound".AsByteArray());
+            key.Concat(i);
+            key.Concat(j);
+            Storage.Put(Storage.CurrentContext, key, color);
+            Colored(i.AsBigInteger(), j.AsBigInteger(), color);
+            return true;
         }
 
-        private static byte[] Create()
+        private static byte[] GetColorRGB(byte[] i, byte[] j)
         {
-            byte[] data = new byte[3000000];
-            for (int i = 0; i < data.Length; i += 3)
-            {
-                data[i] = (byte)255;
-                data[i + 1] = (byte)255;
-                data[i + 2] = (byte)255;
-            }
-            // criando o tabuleiro todo branco
-            Storage.Put(Storage.CurrentContext, "FirstBattleGound".AsByteArray(), data);
-            return null;
+            if (i.AsBigInteger() > 3000 || j.AsBigInteger() > 3000)
+                return null;
+
+            byte[] key = new byte[] { };
+            key.Concat("FirstBattleGound".AsByteArray());
+            key.Concat(i);
+            key.Concat(j);
+            return Storage.Get(Storage.CurrentContext, key);
         }
     }
 }
