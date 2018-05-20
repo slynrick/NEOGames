eb57009db95b992ea5f5736790e84ecfb3237605
Fabio Henrique Cardoso <slynrick@gmail.com>
2018-05-20 11:30:21 -0300

    Put all data in only one storage

--- NEOPixelBattle/NEOPixelBattle/pixelbattle.cs
+++ NEOPixelBattle/NEOPixelBattle/pixelbattle.cs
@@ -8,52 +8,113 @@ namespace Neo.SmartContract
 {
     public class NEOPixelBattle : Framework.SmartContract
     {
-        [DisplayName("colored")]
-        public static event Action<BigInteger,BigInteger,byte[]> Colored;
+        private static readonly byte[] FirstBattleGroundKey = "FirstBattleGround".AsByteArray();
+        private static readonly byte[] DefaultColor = "#ffffff".AsByteArray();
+
+        private static readonly int Rows = 3000;
+        private static readonly int Columns = 3000;
 
         public static object Main(string operation, params object[] args)
         {
+            if (operation == "StartTheGame()")
+            {
+                if (args.Length != 0) return false;
+                return StartTheGame();
+            }
+
+            if (operation == "GetAllBattleGround()")
+            {
+                if (args.Length != 0) return false;
+                return GetAllBattleGround();
+            }
+
             if ( operation == "SetColorRGB()")
             {
                 if (args.Length != 3) return false;
-                return SetColorRGB((byte[])args[0], (byte[])args[1], (byte[])args[2]);
+                return SetColorRGB((int)args[0], (int)args[1], ((string)args[2]).AsByteArray());
             }
 
             if (operation == "GetColorRGB()")
             {
                 if (args.Length != 2) return false;
-                return GetColorRGB((byte[])args[0], (byte[])args[1]);
+                return GetColorRGB((int)args[0], (int)args[1]);
             }
 
             return false;
         }
 
-        private static bool SetColorRGB(byte[] i, byte[] j, byte[] color)
+        // it can only be done once, it was made to start the game after deploy it
+        private static bool StartTheGame()
         {
-            if (color.Length != 3)
+            byte[] data = Storage.Get(Storage.CurrentContext, FirstBattleGroundKey);
+
+            // checking if the data was initialized, it will be done only once
+            if (data.Length != Rows * Columns * DefaultColor.Length)
+            {
+                data = new byte[] { };
+                for (int i = 0; i < Rows * Columns; ++i)
+                    data.Concat(DefaultColor); // initializing with the white color
+                return true;
+            }
+
+            return false;
+        }
+
+        // function resposible for set color to a pixel
+        // the color must be in hex like #4286f4
+        private static bool SetColorRGB(int i, int j, byte[] color)
+        {
+            if (color.Length != DefaultColor.Length)
                 return false;
-            if (i.AsBigInteger() > 3000 || j.AsBigInteger() > 3000)
+            if (!CheckIndex(i, j))
                 return false;
 
-            byte[] key = new byte[] { };
-            key.Concat("FirstBattleGround".AsByteArray());
-            key.Concat(i);
-            key.Concat(j);
-            Storage.Put(Storage.CurrentContext, key, color);
-            Colored(i.AsBigInteger(), j.AsBigInteger(), color);
+            byte[] data = Storage.Get(Storage.CurrentContext, FirstBattleGroundKey);
+
+            // checking if the data was initialized, it will be done only once
+            if (data.Length != Rows * Columns * DefaultColor.Length)
+                return false;
+
+            // looking for the right index
+            int index = GetOneDimIndex(i, j);
+            for (int l = 0; l < color.Length; ++l)
+                data[index + l] = color[l];
+
+            Storage.Put(Storage.CurrentContext, FirstBattleGroundKey, data);
+
             return true;
         }
 
-        private static byte[] GetColorRGB(byte[] i, byte[] j)
+        // get a color from a specific pixel
+        private static byte[] GetColorRGB(int i, int j)
         {
-            if (i.AsBigInteger() > 3000 || j.AsBigInteger() > 3000)
+            if (!CheckIndex(i,j))
                 return null;
 
-            byte[] key = new byte[] { };
-            key.Concat("FirstBattleGround".AsByteArray());
-            key.Concat(i);
-            key.Concat(j);
-            return Storage.Get(Storage.CurrentContext, key);
+            byte[] data = Storage.Get(Storage.CurrentContext, FirstBattleGroundKey);
+            return data.Range(GetOneDimIndex(i, j), DefaultColor.Length);
+        }
+
+        // get all pixel data
+        private static byte[] GetAllBattleGround()
+        {
+            return Storage.Get(Storage.CurrentContext, FirstBattleGroundKey);
+        }
+
+        // get the one dimension index
+        private static int GetOneDimIndex(int i, int j)
+        {
+            return (i * Columns * DefaultColor.Length) + (j * DefaultColor.Length);
+        }
+
+        // get the one dimension index
+        private static bool CheckIndex(int i, int j)
+        {
+            if (i < 0 || j < 0)
+                return false;
+            if (i > Rows || j > Columns)
+                return false;
+            return true;
         }
     }
 }
