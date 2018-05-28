8c77c12ec190f2c2d99d0c681694776b9942a9d2
Fabio Henrique Cardoso <slynrick@gmail.com>
2018-05-27 21:31:23 -0300

    update comments

--- NEOPixelBattle/NEOPixelBattle/pixelbattle.cs
+++ NEOPixelBattle/NEOPixelBattle/pixelbattle.cs
@@ -46,10 +46,9 @@ namespace Neo.SmartContract
             return false;
         }
 
-        /*
-             This method starts the game
-             It can only be called one time since the deploy
-        */
+
+        /// <summary>This method starts the game.It can only be called one time since the deploy</summary>
+        /// <returns>Return true if the game was started in this invocation or false if it was alreay started before</returns>
         private static bool StartTheGame()
         {
             byte[] data = Storage.Get(Storage.CurrentContext, FirstBattleGroundKey);
@@ -66,10 +65,12 @@ namespace Neo.SmartContract
             return false;
         }
 
-        /*
-            Function resposible for set color to a pixel
-            The color must be in hex like #4286f4
-        */
+        /// <summary>Function resposible for set color to a pixel</summary>
+        /// <param name="i">Is the row number of the matrix</param>
+        /// <param name="j">Is the column number of the matrix</param>
+        /// <param name="color">Is the color in hex, like #4286f4. Even the method use byte[] you invoke it as string</param>
+        /// <param name="address">The address that invokes the contract method. Use this to check if it's the real caller and to keep the information of the last gamer</param>
+        /// <returns>Return true if the pixel select was painted or false otherwise</returns>
         private static bool SetColorRGB(int i, int j, byte[] color, byte[] address)
         {
             //checking if the address is the same of caller's address
@@ -82,8 +83,9 @@ namespace Neo.SmartContract
             if (address == LastAddress)
                 return false;
 
-            if (color.Length != DefaultColor.Length)
+            if (!EnsureHexColor(color))
                 return false;
+     
             if (!CheckIndex(i, j))
                 return false;
 
@@ -107,9 +109,10 @@ namespace Neo.SmartContract
             return true;
         }
 
-        /*
-            Get a color from a specific pixel
-        */
+        /// <summary>Get a color from a specific pixel</summary>
+        /// <param name="i">Is the row number of the matrix</param>
+        /// <param name="j">Is the column number of the matrix</param>
+        /// <returns>Return the color of the selected pixel as byte[]</returns>
         private static byte[] GetColorRGB(int i, int j)
         {
             if (!CheckIndex(i,j))
@@ -119,25 +122,26 @@ namespace Neo.SmartContract
             return data.Range(GetOneDimIndex(i, j), DefaultColor.Length);
         }
 
-        /*
-            Get all pixel data
-        */
+        /// <summary>Get all pixel data</summary>
+        /// <returns>Return the color of all pixels as byte[](one dimension)</returns>
         private static byte[] GetAllBattleGround()
         {
             return Storage.Get(Storage.CurrentContext, FirstBattleGroundKey);
         }
 
-        /*
-            Get the one dimension index
-        */
+        /// <summary>Get the one dimension index ( not accessible for invocation )</summary>
+        /// <param name="i">Is the row number of the matrix</param>
+        /// <param name="j">Is the column number of the matrix</param>
+        /// <returns>Return the one dimension index of the matrix</returns>
         private static int GetOneDimIndex(int i, int j)
         {
             return (i * Columns * DefaultColor.Length) + (j * DefaultColor.Length);
         }
 
-        /*
-            Check if the index is valid
-        */
+        /// <summary>Check if the index is valid ( not accessible for invocation )</summary>
+        /// <param name="i">Is the row number of the matrix</param>
+        /// <param name="j">Is the column number of the matrix</param>
+        /// <returns>Return true if it's valid or false otherwise</returns>
         private static bool CheckIndex(int i, int j)
         {
             if (i < 0 || j < 0)
@@ -147,12 +151,41 @@ namespace Neo.SmartContract
             return true;
         }
 
-        /*
-            Check if the caller is who claims to be
-        */
+        /// <summary>Check if the caller is who claims to be ( not accessible for invocation )</summary>
+        /// <param name="address">The address that invokes the contract method</param>
+        /// <returns>Return true if it's verified or false otherwise</returns>
         private static bool VerifyWitness(byte[] address)
         {
             return Runtime.CheckWitness(address);
+        }
+
+        /*
+            
+        */
+        /// <summary>Ensure hex color ( not accessible for invocation )</summary>
+        /// <param name="color">Is the color in byte[], like #4286f4.</param>
+        /// <returns>Return true if it's a real hex color or false otherwise</returns>
+        private static bool EnsureHexColor(byte[] color)
+        {
+            if (color.Length != DefaultColor.Length)
+                return false;
+
+            if (color[0] == '#')
+                return false;
+
+            for ( int i = 1; i < color.Length; ++i)
+            {
+                if (color[i] >= '0' && color[i] <= '9')
+                    continue;
+                else if (color[i] >= 'a' && color[i] <= 'f')
+                    continue;
+                else if (color[i] >= 'A' && color[i] <= 'F')
+                    continue;
+                else
+                    return false;
+            }
+
+            return true;
         }
     }
 }
